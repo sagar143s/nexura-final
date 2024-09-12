67a30a0f8b102a8b1e9e793d02324f2c1b8f4c9b
Rohith Sagar <rohithsagar@192-165-0-136.customer.minitel.se>
2024-09-13 02:47:47 +0400

    fact5

--- components/headers/Header1Multipage.jsx
+++ components/headers/Header1Multipage.jsx
@@ -15,17 +15,17 @@ export default function Header1Multipage({ links }) {
       <div className="nav-logo-wrap local-scroll">
         <Link href={`/`} className="logo">
           <Image
-            src="/assets/images/logo-dark.svg"
+            src="/assets/images/logo.png"
             alt="Your Company Logo"
-            width={105}
-            height={34}
+            width={180}
+            height={50}
             className="light-mode-logo"
           />
           <Image
-            src="/assets/images/logo-white.svg"
+            src="/assets/images/logowhite.png"
             alt="Your Company Logo"
-            width={105}
-            height={34}
+            width={180}
+            height={50}
             className="dark-mode-logo"
           />
         </Link>
