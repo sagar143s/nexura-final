c2773901ffa6f0a19ee21cfd2c179434990139ce
Rohith Sagar <rohithsagar@192-165-0-136.customer.minitel.se>
2024-09-14 14:27:53 +0400

    clinets

--- components/homes/home-1/Brands2.jsx
+++ components/homes/home-1/Brands2.jsx
@@ -1,50 +1,61 @@
 import React from "react";
 import Image from "next/image";
+
 export default function Brands2() {
   return (
     <div className="container position-relative">
       <div className="row">
         <div className="col-md-8 offset-md-2 text-center">
-          <h2 className="section-title-tiny mb-30">
-            Trusted by Leading Companies
-          </h2>
+          <h1 className="section-title-tiny mb-30">
+          Our Esteemed Clients
+          </h1>
 
           <div className="logo-grid">
-            <Image
-              className="logo-grid-img"
-              src="/assets/images/clients-logos/logo-grid/logo-1-white.png"
-              width="110"
-              height="33"
-              alt="Image description is here"
-            />
-            <Image
-              className="logo-grid-img"
-              src="/assets/images/clients-logos/logo-grid/logo-2-white.png"
-              width="119"
-              height="33"
-              alt="Image description is here"
-            />
-            <Image
-              className="logo-grid-img"
-              src="/assets/images/clients-logos/logo-grid/logo-3-white.png"
-              width="79"
-              height="33"
-              alt="Image description is here"
-            />
-            <Image
-              className="logo-grid-img"
-              src="/assets/images/clients-logos/logo-grid/logo-4-white.png"
-              width="122"
-              height="33"
-              alt="Image description is here"
-            />
-            <Image
-              className="logo-grid-img"
-              src="/assets/images/clients-logos/logo-grid/logo-5-white.png"
-              width="100"
-              height="33"
-              alt="Image description is here"
-            />
+            <a href="https://taseesrealestate.ae" target="_blank" rel="noopener noreferrer">
+              <Image
+                className="logo-grid-img"
+                src="/assets/images/clients-logos/logo-grid/tasees.png"
+                width="150"
+                height="43"
+                alt="Tasees"
+              />
+            </a>
+            <a href="https://bullandbearz.com" target="_blank" rel="noopener noreferrer">
+              <Image
+                className="logo-grid-img"
+                src="/assets/images/clients-logos/logo-grid/bullandbearz.png"
+                width="150"
+                height="43"
+                alt="Logo 2"
+              />
+            </a>
+            <a href="https://masca.ae" target="_blank" rel="noopener noreferrer">
+              <Image
+                className="logo-grid-img"
+                src="/assets/images/clients-logos/logo-grid/masca.jpg"
+                width="150"
+                height="43"
+                alt="Logo 3"
+              />
+            </a>
+            {/* <a href="https://www.example4.com" target="_blank" rel="noopener noreferrer">
+              <Image
+                className="logo-grid-img"
+                src="/assets/images/clients-logos/logo-grid/logo-4-white.png"
+                width="122"
+                height="33"
+                alt="Logo 4"
+              />
+            </a>
+            <a href="https://www.example5.com" target="_blank" rel="noopener noreferrer">
+              <Image
+                className="logo-grid-img"
+                src="/assets/images/clients-logos/logo-grid/logo-5-white.png"
+                width="100"
+                height="33"
+                alt="Logo 5"
+              />
+            </a> */}
           </div>
         </div>
       </div>
