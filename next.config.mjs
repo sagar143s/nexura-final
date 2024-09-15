db89b10046252371de4866b4fc4be26f920f2e0d
Rohith Sagar <rohithsagar@192-165-0-136.customer.minitel.se>
2024-09-15 23:41:57 +0400

    google analytics

--- next.config.mjs
+++ next.config.mjs
@@ -2,3 +2,13 @@
 const nextConfig = {};
 
 export default nextConfig;
+
+
+// const sitemap = require('next-sitemap');
+
+// module.exports = {
+//   sitemap: {
+//     siteUrl: 'https://nexura.ae',
+//     generateRobotsTxt: true,
+//   },
+// };
