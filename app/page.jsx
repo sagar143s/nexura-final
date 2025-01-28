f465e9f2ba1b7a5f18fb353d77cccd5e2f979ece
Rohith Sagar <bullandbearzdb@gmail.com>
2025-01-28 16:56:52 +0400

    chang

--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,9 @@
 import Header1 from "@/components/headers/Header1";
 import Home1 from "@/components/homes/home-1";
 import dynamic from "next/dynamic";
-import { Analytics } from "@vercel/analytics/react"
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+
 const ParallaxContainer = dynamic(
   () => import("@/components/common/ParallaxContainer"),
   {
@@ -16,8 +17,10 @@ import Footer1 from "@/components/footers/Footer1";
 
 export const metadata = {
   title: "SquareCom IT Solutions | Dubai's Leading IT Experts",
-  description: "SquareCom IT Solutions offers innovative IT services in Dubai. We specialize in software development, networking, cybersecurity, and digital transformation. Partner with us for cutting-edge technology solutions tailored to your business.",
-  keywords: "SquareCom IT Solutions, Dubai IT company, IT services Dubai, software development Dubai, networking services UAE, cybersecurity Dubai, digital transformation UAE, cloud services Dubai, IT consulting Dubai, web development Dubai, mobile app development Dubai, IT support Dubai, managed IT services, technology solutions Dubai, custom software solutions, business IT solutions"
+  description:
+    "SquareCom IT Solutions offers innovative IT services in Dubai. We specialize in software development, networking, cybersecurity, and digital transformation. Partner with us for cutting-edge technology solutions tailored to your business.",
+  keywords:
+    "SquareCom IT Solutions, Dubai IT company, IT services Dubai, software development Dubai, networking services UAE, cybersecurity Dubai, digital transformation UAE, cloud services Dubai, IT consulting Dubai, web development Dubai, mobile app development Dubai, IT support Dubai, managed IT services, technology solutions Dubai, custom software solutions, business IT solutions",
 };
 
 export default function Home1MainDemoMultiPageDark() {
@@ -32,6 +35,14 @@ export default function Home1MainDemoMultiPageDark() {
         })(window,document,'script','dataLayer','GTM-W594CQK8');`}
       </script>
       {/* End Google Tag Manager */}
+
+      {/* Trustpilot Script */}
+      <script>
+        {`(function(w,d,s,r,n){w.TrustpilotObject=n;w[n]=w[n]||function(){(w[n].q=w[n].q||[]).push(arguments)};
+            a=d.createElement(s);a.async=1;a.src=r;a.type='text/java'+s;f=d.getElementsByTagName(s)[0];
+            f.parentNode.insertBefore(a,f)})(window,document,'script', 'https://invitejs.trustpilot.com/tp.min.js', 'tp');
+            tp('register', 'w59XOlGTvbTZs1im');`}
+      </script>
 
       <div className="dark-mode">
         <div className="theme-main">
